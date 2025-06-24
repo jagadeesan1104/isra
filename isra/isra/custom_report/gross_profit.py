59f9a7c5534257eadb30f8c95402b734c176c9f3
Arun <arun.r@riverstonetech.com>
2025-06-24 21:10:03 +0530

    Payment reciept

--- isra/isra/custom_report/gross_profit.py
+++ isra/isra/custom_report/gross_profit.py
@@ -11,9 +11,12 @@ def export_gross_profit_excel():
 	data = run("Gross Profit", form_params.filters, are_default_filters=False)
 	data = frappe._dict(data)
 	total = data.get("result")[-1] if len(data.get("result")) > 0 else []
+	total["parent_invoice"] = total.get("sales_invoice")
+	del total["sales_invoice"]
 	records = data.get("result")[:-1]
 	column_list = data.get("columns")
-	frappe.log_error(message=f"{records}", title="Test")
+	column_list[0] = {"fieldname": "parent_invoice", "label": "Sales Invoice"}
+	column_list.insert(1, {"fieldname": "item_code", "label": "Item"})
 	columns = [column.get("fieldname") for column in column_list]
 	labels = {col.get("fieldname"):col.get("label") for col in column_list}
 	records = [row for row in records if row.get("indent") == 1]
@@ -22,14 +25,14 @@ def export_gross_profit_excel():
 		for key in list(row.keys()):
 			if key not in columns:
 				del row[key]
-			elif row[key] is None or isinstance(row[key], date):
-				row[key] = "" if row[key] is None else row[key].strftime('%d-%m-%Y')
+			elif row[key] is None:
+				row[key] = ""
+			elif isinstance(row[key], date):
+				row[key] = row[key].strftime('%d-%m-%Y')
 
-	# for key in columns:
-	# 	total.setdefault(key, "")
 	records.append(total)
 	records_dict = {}
-	records_dict = {key: [row[key] if isinstance(row, dict) else row[index] for row in records] for index, key in enumerate(columns)}
+	records_dict = {key: [row.get(key) if isinstance(row, dict) else row[index] for row in records] for index, key in enumerate(columns)}
 	df = pd.DataFrame(records_dict)
 	df.rename(columns=labels, inplace=True)
 
@@ -37,7 +40,7 @@ def export_gross_profit_excel():
 	with pd.ExcelWriter(output, engine='openpyxl') as writer:
 		df.to_excel(writer, index=False, sheet_name='Report')
 
-	frappe.local.response.filename = "report_data.xlsx"
+	frappe.local.response.filename = "Gross Profit.xlsx"
 	frappe.local.response.filecontent = output.getvalue()
 	frappe.local.response.type = "binary"
 	# except Exception as err:
