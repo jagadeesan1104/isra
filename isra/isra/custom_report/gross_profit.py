bcda33c0c8fef0ea26afbe8ba17872c97e90718b
Arun <arun.r@riverstonetech.com>
2025-06-24 21:43:19 +0530

    Payment reciept

--- isra/isra/custom_report/gross_profit.py
+++ isra/isra/custom_report/gross_profit.py
@@ -11,12 +11,13 @@ def export_gross_profit_excel():
 	data = run("Gross Profit", form_params.filters, are_default_filters=False)
 	data = frappe._dict(data)
 	total = data.get("result")[-1] if len(data.get("result")) > 0 else []
-	total["parent_invoice"] = total.get("sales_invoice")
-	del total["sales_invoice"]
+	# total["parent_invoice"] = total.get("sales_invoice")
+	# del total["sales_invoice"]
 	records = data.get("result")[:-1]
 	column_list = data.get("columns")
 	column_list[0] = {"fieldname": "parent_invoice", "label": "Sales Invoice"}
 	column_list.insert(1, {"fieldname": "item_code", "label": "Item"})
+	column_list.pop()
 	columns = [column.get("fieldname") for column in column_list]
 	labels = {col.get("fieldname"):col.get("label") for col in column_list}
 	records = [row for row in records if row.get("indent") == 1]
@@ -32,7 +33,14 @@ def export_gross_profit_excel():
 
 	records.append(total)
 	records_dict = {}
-	records_dict = {key: [row.get(key) if isinstance(row, dict) else row[index] for row in records] for index, key in enumerate(columns)}
+	records_dict = {}
+	for index, key in enumerate(columns):
+		records_dict[key] = []
+		for row in records:
+			if isinstance(row, dict):
+				records_dict[key].append(row.get(key))
+			else:
+				records_dict[key].append(row[index])
 	df = pd.DataFrame(records_dict)
 	df.rename(columns=labels, inplace=True)
 
